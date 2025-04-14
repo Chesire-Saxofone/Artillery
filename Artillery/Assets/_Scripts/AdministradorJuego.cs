88093d3531daaa58e452f0e6da75e517ca51f61f
ChesireSaxofone <josefeospina2014@gmail.com>
2025-04-14 17:04:36 -0500

    Entrega Modulo 16
    Agrego los ejercicios requeridos para el modulo 16

--- Artillery/Assets/_Scripts/AdministradorJuego.cs
+++ Artillery/Assets/_Scripts/AdministradorJuego.cs
@@ -4,11 +4,30 @@ using System.Collections;
 
 public class AdministradorJuego : MonoBehaviour
 {
-    public static AdministradorJuego SingletonAdministradorJuego;
-    public static int VelocidadBola = 30;
-    public static int DisparosPorJuego = 10;
-    public static float VelocidadRotacion = 1;
+    private static AdministradorJuego SingletonAdministradorJuego;
+    private static int VelocidadBola = 30;
+    private static int DisparosPorJuego = 10;
+    private static float VelocidadRotacion = 1;
 
+    public int VelocidadBolaPublico
+    {
+        get { return VelocidadBola; }
+        set { VelocidadBola = value; }
+    }
+    public int DisparosPorJuegoPublico
+    {
+        get { return DisparosPorJuego; }
+        set { DisparosPorJuego = value; }
+    }
+    public float VelocidadRotacionPublico
+    {
+        get { return VelocidadRotacion; }
+        set { VelocidadRotacion = value; }
+    }
+    public static AdministradorJuego ObtenerInstancia()
+    {
+        return SingletonAdministradorJuego;
+    }
     private void Awake()
     {
         if (SingletonAdministradorJuego == null)
--- Artillery/Assets/_Scripts/Canon.cs
+++ Artillery/Assets/_Scripts/Canon.cs
@@ -4,23 +4,40 @@ using UnityEngine;
 
 public class Canon : MonoBehaviour
 {
+    
+
     [SerializeField] private GameObject BalaPrefab;
     private GameObject puntaCanon;
     private GameObject adminJuego;
     private float rotacion;
+    private int disparos;
+    private AdministradorJuego admin;
+
 
     private void Start()
     {
         puntaCanon = transform.Find("PuntaCanon").gameObject;
 
+        admin = AdministradorJuego.ObtenerInstancia();
+
+        if (admin != null)
+        {
+            disparos = admin.DisparosPorJuegoPublico;
+            rotacion = 0;
+        }
+        else
+        {
+            Debug.LogError("No se pudo obtener la instancia del AdministradorJuego");
+        }
+
     }   
     // Update is called once per frame
     void Update()
     {
-        rotacion += Input.GetAxis("Horizontal") * AdministradorJuego.VelocidadRotacion;
+        rotacion += Input.GetAxis("Horizontal") * admin.VelocidadRotacionPublico;
         if (rotacion <= 90 && rotacion >= 0)
         {
-            transform.eulerAngles = new Vector3(rotacion, 45,0.0f);
+            transform.eulerAngles = new Vector3(rotacion, 90.0f, 0.0f);
         }
 
         if (rotacion > 90)
@@ -32,13 +49,17 @@ public class Canon : MonoBehaviour
             rotacion = 0;
         }
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (disparos > 0)
         {
-            GameObject temp = Instantiate(BalaPrefab, puntaCanon.transform.position, puntaCanon.transform.rotation);
-            Rigidbody tempRB = temp.GetComponent<Rigidbody>();
-            Vector3 direccionDisparo = transform.rotation.eulerAngles;
-            direccionDisparo.y = 90 - direccionDisparo.x;
-            tempRB.linearVelocity = direccionDisparo.normalized * AdministradorJuego.VelocidadBola;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                GameObject temp = Instantiate(BalaPrefab, puntaCanon.transform.position, transform.rotation);
+                Rigidbody tempRB = temp.GetComponent<Rigidbody>();
+                Vector3 direccionDisparo = transform.rotation.eulerAngles;
+                direccionDisparo.y = 90 - direccionDisparo.x;
+                tempRB.linearVelocity = direccionDisparo.normalized * admin.VelocidadBolaPublico;
+                disparos--;
+            }
         }
     }
 }
